936bdca3f89bc7a12700017d3a832747b1b58344
André Alvim <andre@alvim.net>
2024-01-14 21:19:24 -0300

    fix: remove unused import

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,11 +1,9 @@
-import { User } from '@prisma/client'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { RegisterUseCase } from '@/use-cases/register'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
-import { error } from 'console'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
