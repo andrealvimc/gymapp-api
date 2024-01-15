949e1e297c0caaee7bead322e6ff05dd059047a1
André Alvim <andre@alvim.net>
2024-01-14 21:00:43 -0300

    feat: add inversao de dependencias

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,7 +1,6 @@
 import { hash } from 'bcryptjs'
 
 import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
 interface RegisterUseCaseParams {
   name: string
@@ -9,28 +8,26 @@ interface RegisterUseCaseParams {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseParams) {
-  const password_hash = await hash(password, 7)
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
+  async execute({ name, email, password }: RegisterUseCaseParams) {
+    const password_hash = await hash(password, 7)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('User with same email already exists')
+    }
+
+    await this.usersRepository.create({
       email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    throw new Error('User with same email already exists')
+      name,
+      password_hash,
+    })
   }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
-    email,
-    name,
-    password_hash,
-  })
 }
